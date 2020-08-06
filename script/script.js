d9eeca117383ea8eb497b3af9d655aba6bbbf406
Nikita <nike-super@bk.ru>
2020-08-06 10:50:24 +0300

    для прототипа переделал на ES5

--- script/script.js
+++ script/script.js
@@ -1,5 +1,39 @@
-class Laptop {
-  constructor(
+function Laptop(
+  name,
+  model,
+  serialNumber,
+  yearOfManufacture,
+  ramSize,
+  romSize,
+  mass,
+  haveDisk,
+) {
+  this.name = name;
+  this.model = model;
+  this.serialNumber = serialNumber;
+  this.yearOfManufacture = yearOfManufacture;
+  this.ramSize = ramSize;
+  this.romSize = romSize;
+  this.mass = mass;
+  this.haveDisk = haveDisk;
+  this.getRomSize = () => {
+    return this.romSize;
+  };
+}
+Laptop.prototype.getRamSize = function () {
+  return this.ramSize;
+};
+function Ultrabook(
+  name,
+  model,
+  serialNumber,
+  yearOfManufacture,
+  ramSize,
+  romSize,
+  mass,
+) {
+  Laptop.call(
+    this,
     name,
     model,
     serialNumber,
@@ -7,45 +41,15 @@ class Laptop {
     ramSize,
     romSize,
     mass,
-    haveDisk,
-  ) {
-    this.name = name;
-    this.model = model;
-    this.serialNumber = serialNumber;
-    this.yearOfManufacture = yearOfManufacture;
-    this.ramSize = ramSize;
-    this.romSize = romSize;
-    this.mass = mass;
-    this.haveDisk = haveDisk;
-  }
-
-  getRamSize() {
-    return this.ramSize;
-  }
-  getRomSize() {
-    return this.ramSize;
-  }
+  );
+  if (this.mass <= 0 || this.mass > 1.5) {
+    throw "Масса не должна быть более 1.5 кг";
+  } else this.mass = mass;
 }
-
-class Ultrabook extends Laptop {
-  constructor(
-    name,
-    model,
-    serialNumber,
-    yearOfManufacture,
-    ramSize,
-    romSize,
-    mass,
-  ) {
-    super(name, model, serialNumber, yearOfManufacture, ramSize, romSize, mass);
-    if (this.mass <= 0 || this.mass > 1.5) {
-      throw "Масса не должна быть более 1.5 кг";
-    } else this.mass = mass;
-  }
-  getName() {
-    return this.name;
-  }
-  getYear() {
-    return this.yearOfManufacture;
-  }
-}
+Ultrabook.prototype = Object.create(Laptop.prototype);
+Ultrabook.prototype.getName = function () {
+  return this.name;
+};
+Ultrabook.prototype.getYear = function () {
+  return this.yearOfManufacture;
+};
